67e94e20a62ca784a4d6a65bdc94793e071ff0cf
nqnil <nqnil@hotmail.com>
2019-02-27 19:23:26 +0100

    a ver

---
+++ java2/hola.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hola {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        Scanner texto=new Scanner(System.in);
+        System.out.println("dime algo:"+texto.nextLine());
+
+    }
+}
